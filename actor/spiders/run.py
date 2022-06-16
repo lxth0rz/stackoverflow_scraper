9d3cdbab1c85f177590bb1a1cb908d5414479f99
Administrator+1-at-592373886397 <ahsaeldin@gmail.com>
2022-06-16 20:44:52 +0200

    added scrapy_by input
    added scrapy_by input

--- actor/spiders/run.py
+++ actor/spiders/run.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+
+import os
+import apify
+import logging
 from scrapy import Spider
 from urllib.parse import urljoin
+from apify_client import ApifyClient
 from scrapy.http.request import Request
 
 
@@ -8,20 +13,50 @@ class IMDBCreatorsScraper(Spider):
 
     name = 'stackoverflow-users-urls'
 
-    start_overview_url = 'https://stackoverflow.com/users?page=1&tab=reputation&filter=all'
+    logger = None
+
+    scrapy_by_urls = {'rep': 'https://stackoverflow.com/users?tab=reputation',
+                      'new_users': 'https://stackoverflow.com/users?tab=newusers',
+                      'voters': 'https://stackoverflow.com/users?tab=voters',
+                      'editors': 'https://stackoverflow.com/users?tab=editors',
+                      'moderators': 'https://stackoverflow.com/users?tab=moderators', }
+
+    scrapy_by_url = scrapy_by_urls['editors']
 
     def start_requests(self):
-        yield Request(url=self.start_overview_url,
+
+        self.logger = logging.getLogger()
+
+        directory_path = os.getcwd()
+
+        if 'upwork_2' not in directory_path:
+
+            # Initialize the main ApifyClient instance
+            client = ApifyClient(os.environ['APIFY_TOKEN'], api_url=os.environ['APIFY_API_BASE_URL'])
+
+            # Get the resource subclient for working with the default key-value store of the actor
+            default_kv_store_client = client.key_value_store(os.environ['APIFY_DEFAULT_KEY_VALUE_STORE_ID'])
+
+            # Get the value of the actor input and print it
+            self.logger.info('Loading input...')
+            actor_input = default_kv_store_client.get_record(os.environ['APIFY_INPUT_KEY'])['value']
+            self.logger.info(actor_input)
+
+            self.scrapy_by_url = self.scrapy_by_urls[actor_input["scrapeUsersBy"]]
+            self.logger.info(self.scrapy_by_url)
+
+        yield Request(url=self.scrapy_by_url,
                       callback=self.parse_overview)
 
     def parse_overview(self, response):
 
-        users = response.xpath('.//div[@class="user-details"]/a/@href')
+        users = response.xpath('.//div[@class="user-details"]/a')
         if users and len(users) > 0:
-            users = users.extract()
-            users = [urljoin(response.url, x.strip()) for x in users]
             for user in users:
-                print(user)
+                user_url = urljoin(response.url, user.xpath('@href').extract()[0].strip())
+                user_name = user.xpath('text()').extract()[0].strip()
+                yield {'Name': user_name,
+                       'URL': user_url}
 
         next_url = response.xpath('.//a[@rel="next"]/@href')
         if next_url and len(next_url) > 0:
